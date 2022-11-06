16c3a5badd7bc75b9cc7ff299c45173b5993647e
Sidney François <sidney.fvm.dev@outlook.com>
2022-11-05 22:28:48 -0300

    Passing item elements through props

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,34 @@
-const title = "React";
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const welcome = {
-  greetings: "Hey",
-  title: "React",
+  return (
+    <div>
+      <h1>My hacker stories</h1>
+      <Search />
+
+      <hr />
+
+      <List list={stories} />
+    </div>
+  );
 };
-
-function getTitle(title) {
-  return title;
-}
-
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const App = () => (
-  <div>
-    <h1>My hacker stories</h1>
-    <Search />
-
-    <hr />
-
-    <List />
-  </div>
-);
 
 const Search = () => {
   const handleChange = (event) => {
@@ -53,16 +44,22 @@ const Search = () => {
   );
 };
 
-const List = () => (
-  <>
-    <h2>List of items</h2>
-    <Item />
-  </>
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
 );
 
-const Item = () => (
+const Item = ({ item }) => (
   <>
-    <h4>item only</h4>
+    <li>
+      <span>{item.title}</span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.url}</span>
+    </li>
   </>
 );
 
