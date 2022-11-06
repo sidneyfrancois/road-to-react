bb8ecad973285805cdad3e2988698b36718988eb
Sidney François <sidney.fvm.dev@outlook.com>
2022-11-05 21:20:40 -0300

    Initializing project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const title = "React";
+
+function App() {
+  return (
+    <div>
+      <h2>Hello to {title}</h2>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App;
