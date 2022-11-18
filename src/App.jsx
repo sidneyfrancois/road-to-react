3df3ce9c8ef4ac90614cbaed491c699cf2fc98a1
Sidney François <sidney.fvm.dev@outlook.com>
2022-11-18 19:20:49 -0300

    create callback handler handleRemoveStory to pass intro the List component

--- src/App.jsx
+++ src/App.jsx
@@ -13,30 +13,38 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 };
 
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "");
-
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+  const [stories, setStories] = useState(initialStories);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
   };
 
   const searchedStories = stories.filter((story) => {
@@ -52,13 +60,14 @@ const App = () => {
         value={searchTerm}
         isFocused
         onInputChange={handleSearch}
+        onDelete={deleteFromStories}
       >
         <strong>Search:</strong>
       </InputWithLabel>
 
       <hr />
 
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 };
