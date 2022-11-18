4de3cbc0cead5888fe8ff412f65cb5ae72143323
Sidney François <sidney.fvm.dev@outlook.com>
2022-11-18 19:41:58 -0300

    transform the call to onRemoveItem into a inline handler

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,21 +1,15 @@
-const Item = ({ item, onRemoveItem }) => {
-  const handleRemoveitem = () => {
-    onRemoveItem(item);
-  };
-
-  return (
-    <li>
-      <span>{item.title}</span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.url}</span>
-      <span>
-        <button type="button" onClick={handleRemoveitem}>
-          Dismiss
-        </button>
-      </span>
-    </li>
-  );
-};
+const Item = ({ item, onRemoveItem }) => (
+  <li>
+    <span>{item.title}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.url}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
+  </li>
+);
 
 export default Item;
